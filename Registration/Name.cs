871d58cac3b72cb40b67e46cd55b46ae54748367
unknown <satya-1298>
2023-03-30 21:46:20 +0530

    validate FirstName

---
+++ Registration/Name.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Registration
+{
+    public class Name
+    {
+        public static string REGEX_NAME = "[A-Z]{1}[a-z]{3}$";
+        public bool validateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_NAME);
+        }
+    }
+}
---
+++ Registration/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Registration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Name name = new Name();
+            Console.WriteLine("Enter FirstName:");
+            string n = Console.ReadLine();
+            bool val = name.validateFirstName(n);
+            if (val)
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
