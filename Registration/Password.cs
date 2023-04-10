3c2526d14b0be207574950554f1b13a94aa96546
unknown <satya-1298>
2023-04-10 19:40:40 +0530

    validate with min 8 characters

---
+++ Registration/Password.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Registration
+{
+    public class Password
+    {
+        public static string REGEX_PASSWORD = "^[a-zA-Z0-9]{8, }$";
+        public bool validatepassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
--- Registration/Program.cs
+++ Registration/Program.cs
@@ -4,18 +4,40 @@
     {
         public static void Main(string[] args)
         {
-            Name name = new Name();
-            Console.WriteLine("Enter FirstName:");
-            string n = Console.ReadLine();
-            bool val = name.validateFirstName(n);
-            if (val)
+            //Name name = new Name();
+            //Console.WriteLine("Enter FirstName:");
+            //string n = Console.ReadLine();
+            //bool val = name.validateFirstName(n);
+            //if (val)
+            //{
+            //    Console.WriteLine("valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid");
+            //}
+            //Mobileno mobileno = new Mobileno();
+            //Console.WriteLine("Enter Mobile Number:");
+            //string n1 = (Console.ReadLine());
+            //bool val3 = mobileno.validatePhoneno(n1);
+            //if (val3)
+            //{
+            //    Console.WriteLine("valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("incorrect Number");
+            //}
+            Password password = new Password();
+            Console.WriteLine("Enter password");
+            string n2= (Console.ReadLine());
+            bool val1=password.validatepassword(n2);
+            if (val1)
             {
-                Console.WriteLine("valid");
+                Console.WriteLine("Valid");
             }
             else
-            {
-                Console.WriteLine("Invalid");
-            }
+            { Console.WriteLine("In Valid"); }
         }
     }
 }
