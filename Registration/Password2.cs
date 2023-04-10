1cb07fb53c3df7b8055b5ef992f1a4aea31e0712
unknown <satya-1298>
2023-04-10 20:01:35 +0530

    validate password with atleast 1 uppercase

---
+++ Registration/Password2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Registration
+{
+    internal class Password2
+    {
+        public static string Regex_Password2 = "^[A-Z]{1}[a-z0-9]{7}$";
+        public bool validatepassword2(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password2);
+
+        }
+    }
+}
+
