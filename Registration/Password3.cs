d49ca98497c220f4e69507f5d730ae88cb3f23cd
unknown <satya-1298>
2023-04-10 20:09:19 +0530

    Validate password with atleast 1 numeric

---
+++ Registration/Password3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Registration
+{
+    internal class Password3
+    {
+        public static string Regex_Password = "^[A-Z]{1}[0-9]{1}[a-z]{6}$";
+        public bool validatepassword3(string password3)
+        {
+            return Regex.IsMatch(password3, Regex_Password);
+
+        }
+    }
+}
+
