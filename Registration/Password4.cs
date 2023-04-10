498e378e5cb07c838a351eeae3154a71f78ad077
unknown <satya-1298>
2023-04-10 20:30:48 +0530

    validate Password with special character

---
+++ Registration/Password4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Registration
+{
+    public class Password4
+    {
+        public static string Regex_Password = "^(?=[a-zA-Z0-9#@$?-_]{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$";
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
--- Registration/Program.cs
+++ Registration/Program.cs
@@ -4,18 +4,40 @@
     {
         public static void Main(string[] args)
         {
-            Name name = new Name();
-            Console.WriteLine("Enter FirstName:");
-            string n = Console.ReadLine();
-            bool val = name.validateFirstName(n);
-            if (val)
+            //Name name = new Name();
+            //Console.WriteLine("Enter FirstName:");
+            //string n = Console.ReadLine();
+            //bool val = name.validateFirstName(n);
+            //if (val)
+            //{
+            //    Console.WriteLine("valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid");
+            //}
+            //Mobileno mobileno = new Mobileno();
+            //Console.WriteLine("Enter Mobile Number:");
+            //string n1 = (Console.ReadLine());
+            //bool val3 = mobileno.validatePhoneno(n1);
+            //if (val3)
+            //{
+            //    Console.WriteLine("valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("incorrect Number");
+            //}
+            Password4 password = new Password4();
+            Console.WriteLine("Enter password Rule2");
+            string n2= (Console.ReadLine());
+            bool val1=password.ValidatePassword(n2);
+            if (val1)
             {
-                Console.WriteLine("valid");
+                Console.WriteLine("Valid");
             }
             else
-            {
-                Console.WriteLine("Invalid");
-            }
+            { Console.WriteLine("In Valid"); }
         }
     }
 }
