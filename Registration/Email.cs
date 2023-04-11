a798d8ab1fd6192d6f5c5685dafb4b93cef21920
unknown <satya-1298>
2023-04-11 11:04:42 +0530

    Validate Email using regex

---
+++ Registration/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Registration
+{
+    public class Email
+    {
+        public static string REGEX_EMAIL = "[0-9A-Za-z]+[.+-_]{0,1}[0-9A-Za-z]+[@][]A-Za-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+    }
+}
--- Registration/Program.cs
+++ Registration/Program.cs
@@ -4,10 +4,20 @@
     {
         public static void Main(string[] args)
         {
-            Name name = new Name();
-            Console.WriteLine("Enter FirstName:");
+            
+            Email e = new Email();
+            string[] email = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.net", "abc-100@abc.net", "abc.100@abc.com.au","abc+100@gmail.com", };
+            //bool val = e.validateEmail(e);
+            foreach (string v in email)
+            {
+                bool s=e.validateEmail(v);
+                Console.WriteLine(v+" "+s);
+                
+            }
+            Email em = new Email();
+            Console.WriteLine("Enter Email:");
             string n = Console.ReadLine();
-            bool val = name.validateFirstName(n);
+            bool val = em.validateEmail(n);
             if (val)
             {
                 Console.WriteLine("valid");
@@ -16,6 +26,41 @@
             {
                 Console.WriteLine("Invalid");
             }
+
+            //Name name = new Name();
+            //Console.WriteLine("Enter FirstName:");
+            //string n = Console.ReadLine();
+            //bool val = name.validateFirstName(n);
+            //if (val)
+            //{
+            //    Console.WriteLine("valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid");
+            //}
+            //Mobileno mobileno = new Mobileno();
+            //Console.WriteLine("Enter Mobile Number:");
+            //string n1 = (Console.ReadLine());
+            //bool val3 = mobileno.validatePhoneno(n1);
+            //if (val3)
+            //{
+            //    Console.WriteLine("valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("incorrect Number");
+            //}
+            //Password4 password = new Password4();
+            //Console.WriteLine("Enter password Rule4");
+            //string n2= (Console.ReadLine());
+            //bool val1=password.ValidatePassword(n2);
+            //if (val1)
+            //{
+            //    Console.WriteLine("Valid");
+            //}
+            //else
+            //{ Console.WriteLine("In Valid"); }
         }
     }
 }
