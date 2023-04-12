e6a8bf55071e2d6ef42b85c538d9aaaa438814b7
unknown <satya-1298>
2023-04-12 22:11:50 +0530

    Test Method for user validate

--- RegistrationTest/UnitTest1.cs
+++ RegistrationTest/UnitTest1.cs
@@ -5,11 +5,12 @@ namespace RegistrationTest
     [TestClass]
     public class UnitTest1
     {
+        UserValidation validation = new UserValidation();
         [TestMethod]
         public void GivenFirstName_WhenValidate_ShouldReturnValid()
         {
             string name = "Satya";
-            UserValidation validation = new UserValidation();
+            
             var result = validation.ValidateFirstName(name);
             Assert.AreEqual("Valid first name", result);
         }
@@ -17,7 +18,7 @@ namespace RegistrationTest
         public void GivenLastName_WhenValidate_ShouldReturnValid()
         {
             string name = "Adabala";
-            UserValidation validation = new UserValidation();
+            
             string result = validation.ValidateLastName(name);
             Assert.AreEqual("Valid Last name", result);
         }
@@ -25,6 +26,7 @@ namespace RegistrationTest
         public void GivenEmail_WhenValidate_ShouldReturnValid()
         {
             string email ="abc123@gmail.com";
+           
             UserValidation validation = new UserValidation();
             string result = validation.ValidateEmail(email);
             Assert.AreEqual("Valid email", result);
@@ -33,7 +35,7 @@ namespace RegistrationTest
         public void GivenPhoneNumber_WhenValidate_ShouldReturnValid()
         {
             string phone = "+91 7456344556";
-            UserValidation validation = new UserValidation();
+            
             string result = validation.ValidateMobileNumber(phone);
             Assert.AreEqual("Valid mobile number", result);
         }
@@ -41,7 +43,6 @@ namespace RegistrationTest
         public void GivenPassword_WhenValidate_ShouldReturnValid()
         {
             string pwd = "Agtf@1234";
-            UserValidation validation = new UserValidation();
             string result = validation.ValidatePassWord(pwd);
             Assert.AreEqual("Valid password", result);
         }
